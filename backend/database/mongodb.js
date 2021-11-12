d55cf4def52ab4f45fcdfddbd32437461099769c
Dan Deng <hui_danny_deng@hotmail.com>
2021-11-12 16:01:21 -0500

    intial project commit

---
+++ backend/database/mongodb.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb+srv://dandeng:TestMongoDb6367@cluster0.nu9xs.mongodb.net/Ecommerce?retryWrites=true&w=majority'
+};
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { FileUpload } from './fileUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
